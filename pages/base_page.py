9be365010dc5d46c4b9c9cfdc509bb4e17c15588
InbalSchwimmer <inbalschwimmer@gmail.com>
2024-11-07 10:15:36 +0200

    add data folder

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,5 @@
 import time
-
+from data.locators import MenuLocators
 from selenium.common import TimeoutException, NoSuchElementException
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
@@ -11,13 +11,13 @@ class BasePage:
     def __init__(self, driver):
         self.driver: WebDriver = driver
 
-    CHECKOUT_BTN = (By.CSS_SELECTOR, "[data-ga-v2='Checkout']")
-    SORT_BTN = (By.CSS_SELECTOR, "[data-testid='plp-desktop-sort-button']")
-    SORT_BY_PRICE_LOW_HIGH = (By.CSS_SELECTOR, "[data-value='price']")
-    FILTER_BY_COLOR_BTN = (By.CSS_SELECTOR,"[aria-label='Colour']")
-    BLUE_CHECK_BOX = (By.CSS_SELECTOR, "[name='plp-facet-checkbox-colour:blue']")
-    SHOPPING_BAG_BTN = (By.CSS_SELECTOR, "[data-testid='header-shopping-bag']")
-    FAVORITES_BTN = (By.CSS_SELECTOR, ".favourites.header-ogfstg")
+    # CHECKOUT_BTN = (By.CSS_SELECTOR, "[data-ga-v2='Checkout']")
+    # SORT_BTN = (By.CSS_SELECTOR, "[data-testid='plp-desktop-sort-button']")
+    # SORT_BY_PRICE_LOW_HIGH = (By.CSS_SELECTOR, "[data-value='price']")
+    # FILTER_BY_COLOR_BTN = (By.CSS_SELECTOR,"[aria-label='Colour']")
+    # BLUE_CHECK_BOX = (By.CSS_SELECTOR, "[name='plp-facet-checkbox-colour:blue']")
+    # SHOPPING_BAG_BTN = (By.CSS_SELECTOR, "[data-testid='header-shopping-bag']")
+    # FAVORITES_BTN = (By.CSS_SELECTOR, ".favourites.header-ogfstg")
 
     def click(self, locator):
         time.sleep(1)
@@ -32,7 +32,7 @@ class BasePage:
         self.driver.find_element(*locator).send_keys(text)
 
     def sort_page_results(self, locator):
-        self.click(self.SORT_BTN)
+        self.click(MenuLocators.SORT_BTN)
         self.click(locator)
 
     def get_price(self, locator):
--- pages/shopping_bag_page.py
+++ pages/shopping_bag_page.py
@@ -1,35 +1,35 @@
 from selenium.webdriver.common.by import By
-
 from pages.base_page import BasePage
+from data.locators import ShoppingBagLocators
 
 
 class ShoppingBag(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    VIEW_EDIT_BAG_BTN = (By.CSS_SELECTOR, "[data-ga-v3='View Bag']")
-    PRODUCT_QUANTITY = (By.CSS_SELECTOR, ".dk_toggle.dk_label")
-    REMOVE_ITEM_LINK = (By.CSS_SELECTOR, ".sbm-idDeleteButton ")
-    ITEM_COUNT = (By.CSS_SELECTOR, "#itemCount")
-    EMPTY_CART_MSG = (By.CSS_SELECTOR, ".d-lg-block")
-    ITEM_VALUE = (By.XPATH, "//div[@class ='sbm-order-total-price sbm-summary-price-value']")
-    ITEM_QUANTITY_TOGGLE = (By.CSS_SELECTOR, ".dk_toggle")
-    ITEM_QUANTITY_PLUS = (By.CSS_SELECTOR, ".qty-plus")
-    # The locator is a template, and we'll replace '{}' with the actual quantity value dynamically
-    ITEM_QUANTITY_SELECTOR_TEMPLATE = "[data-dk-dropdown-value='{}']"
-    # SHOP_MORE_LINK = (By.CSS_SELECTOR, "div.bottom a.shopmore")
-    SHOP_MORE_LINK = (By.CSS_SELECTOR, ".sbm-idShopMoreButton")
-    SAVE_FOR_LATER_LINK = (By.CSS_SELECTOR, ".sbm-idSaveForLaterButton")
-    SAVE_ITEM_COUNT = (By.CSS_SELECTOR, ".sfl-item-count")
+    # VIEW_EDIT_BAG_BTN = (By.CSS_SELECTOR, "[data-ga-v3='View Bag']")
+    # PRODUCT_QUANTITY = (By.CSS_SELECTOR, ".dk_toggle.dk_label")
+    # REMOVE_ITEM_LINK = (By.CSS_SELECTOR, ".sbm-idDeleteButton ")
+    # ITEM_COUNT = (By.CSS_SELECTOR, "#itemCount")
+    # EMPTY_CART_MSG = (By.CSS_SELECTOR, ".d-lg-block")
+    # ITEM_VALUE = (By.XPATH, "//div[@class ='sbm-order-total-price sbm-summary-price-value']")
+    # ITEM_QUANTITY_TOGGLE = (By.CSS_SELECTOR, ".dk_toggle")
+    # ITEM_QUANTITY_PLUS = (By.CSS_SELECTOR, ".qty-plus")
+    # # The locator is a template, and we'll replace '{}' with the actual quantity value dynamically
+    # ITEM_QUANTITY_SELECTOR_TEMPLATE = "[data-dk-dropdown-value='{}']"
+    # # SHOP_MORE_LINK = (By.CSS_SELECTOR, "div.bottom a.shopmore")
+    # SHOP_MORE_LINK = (By.CSS_SELECTOR, ".sbm-idShopMoreButton")
+    # SAVE_FOR_LATER_LINK = (By.CSS_SELECTOR, ".sbm-idSaveForLaterButton")
+    # SAVE_ITEM_COUNT = (By.CSS_SELECTOR, ".sfl-item-count")
 
     def select_product_quantity(self, quantity):
         # Click the toggle to open the dropdown
-        if self.ITEM_QUANTITY_PLUS:
-            self.click(self.ITEM_QUANTITY_PLUS)
+        if ShoppingBagLocators.ITEM_QUANTITY_PLUS:
+            self.click(ShoppingBagLocators.ITEM_QUANTITY_PLUS)
         else:
-            self.click(self.ITEM_QUANTITY_TOGGLE)
+            self.click(ShoppingBagLocators.ITEM_QUANTITY_TOGGLE)
             # Dynamically create the locator by replacing '{}' with the actual quantity
-            quantity_locator = (By.CSS_SELECTOR, self.ITEM_QUANTITY_SELECTOR_TEMPLATE.format(quantity))
+            quantity_locator = (By.CSS_SELECTOR, ShoppingBagLocators.ITEM_QUANTITY_SELECTOR_TEMPLATE.format(quantity))
             # Now click the specific quantity
             self.click(quantity_locator)
 
